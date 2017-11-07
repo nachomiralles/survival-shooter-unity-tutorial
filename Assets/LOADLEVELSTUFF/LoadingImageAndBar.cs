158b093b2ef87fd7d90313578ba86da383754f95
Nacho Miralles <nachomiralles@gmail.com>
2017-11-07 09:11:09 +0100

    Load Scene with bar.

---
+++ Assets/LOADLEVELSTUFF/LoadingImageAndBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingImageAndBar : MonoBehaviour {
+
+    public Slider loadingBar;
+    public GameObject loadingObject;
+
+    private AsyncOperation async;
+
+    public void LoadLevelAsync(string level)
+    {
+        loadingObject.SetActive(true);
+        StartCoroutine(LoadTheLevel(level));
+    }
+
+    IEnumerator LoadTheLevel(string level)
+    {
+        async = SceneManager.LoadSceneAsync(level);
+        
+        while (!async.isDone)
+        {
+            print(async.progress);
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+}
